de9cda81a734c8cec151c5705637829fec3ef149
Young <jiiiy0510o@gmail.com>
2022-03-23 23:49:44 +0900

    styled-components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: yellow;
+`;
+
+function App() {
+  return (
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </Father>
+  );
+}
+
+export default App;
