3c991fc7a5e248722070668f4460143aef2ec8cf
Young <jiiiy0510o@gmail.com>
2022-03-24 15:48:42 +0900

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
+  height: 100vh;
   display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+const animation = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to{
+  transform: rotate(360deg);
+  border-radius: 50%;
+}
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
-  background-color: yellow;
+`;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: ${(props) => props.theme.backgroundColor};
+  display: felx;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    transform: scale(1.1);
+  }
+  span {
+    color: ${(props) => props.theme.textColor};
+    font-size: 36px;
+    &:hover {
+      font-size: 50px;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>Hello</span>
+      </Box>
+    </Wrapper>
   );
 }
 
