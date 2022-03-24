dcaef7c5aa72e58b1f754652a06558c3dc418d1a
Young <jiiiy0510o@gmail.com>
2022-03-24 23:35:46 +0900

    typescript 공부

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tan" borderColor="#111" />
+    </div>
+  );
+}
+
+export default App;
