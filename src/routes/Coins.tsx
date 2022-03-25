4f582b61e736a3a01f4e0c75e93a19e0684cafc1
Young <jiiiy0510o@gmail.com>
2022-03-26 01:16:04 +0900

    Coin Tracker Home

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Container = styled.div``;
 
@@ -17,60 +18,75 @@ const CoinList = styled.ul`
 const Coin = styled.li`
   background-color: white;
   color: ${(props) => props.theme.bgColor};
-  padding: 26px 20px;
-  margin-bottom: 10px;
-  border-radius: 10px;
+  padding: 22px 20px;
+  margin-bottom: 11px;
+  border: 5px double ${(props) => props.theme.bgColor};
+  border-radius: 12px;
+  font-weight: 900;
+  a {
+    transition: color 0.3s ease-in-out;
+    display: block;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
 `;
 
 const Title = styled.h1`
   letter-spacing: 3px;
   font-weight: 600;
   font-size: 48px;
-  color: ${(props) => props.theme.textColor};
+  color: ${(props) => props.theme.accentColor};
 `;
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-];
+const Loader = styled.span`
+  height: 88vh;
+  color: ${(props) => props.theme.accentColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 26px;
+  font-weight: 900;
+  letter-spacing: 3px;
+`;
+interface ICoin {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 function Coins() {
+  const [coins, setCoins] = useState<ICoin[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 30));
+      setLoading(false);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>COINS</Title>
       </Header>
-      <CoinList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinList>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))}
+        </CoinList>
+      )}
     </Container>
   );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,6 @@ import { DefaultTheme } from "styled-components";
 
 export const Theme: DefaultTheme = {
   bgColor: "#2f3640",
-  textColor: "#4cd137",
-  accentColor: "#44bd332",
+  textColor: "#d7bfdc",
+  accentColor: "#af69ee",
 };
