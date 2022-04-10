35c5348cc0b551c3d8a4e6193f9a465c682d8989
Young <jiiiy0510o@gmail.com>
2022-04-10 23:57:08 +0900

    useMotionValue

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,41 @@
-import Router from "./Router";
-import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+import { motion, useMotionValue, useTransform, useViewportScroll } from "framer-motion";
+import { useEffect } from "react";
 
-const GlobalStyled = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding:wght@700&display=swap');
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed, 
-  figure, figcaption, footer, header, hgroup, 
-  menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-  }
-    article, aside, details, figcaption, figure, 
-  footer, header, hgroup, menu, nav, section {
-    display: block;
-  }
-  * {
-    box-sizing: border-box
-  }
-  body {
-    line-height: 1;
-    font-family: 'Nanum Gothic Coding', monospace;
-    background-color:${(props) => props.theme.bgColor};
-    color:${(props) => props.theme.textColor};
-  }
-  ol, ul {
-    list-style: none;
-  }
-  blockquote, q {
-    quotes: none;
-  }
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: '';
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-  a {
-    text-decoration:none;
-    color: inherit;
-  }
+const Wrapper = styled(motion.div)`
+  width: 100vw;
+  height: 200vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(189, 0, 255));
+`;
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(84, 67, 238), rgb(22, 61, 146))",
+      "linear-gradient(135deg, rgb(255, 208, 0), rgb(255, 242, 157))",
+    ]
+  );
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+
   return (
-    <>
-      <GlobalStyled />
-      <Router />
-    </>
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
+    </Wrapper>
   );
 }
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const Theme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "#d7bfdc",
-  accentColor: "#af69ee",
-};
+export const darkTheme: DefaultTheme = {};
